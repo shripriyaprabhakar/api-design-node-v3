d635f48972b713f198af2d08761846534b3f9f03
priya suresh <priyasuresh@MacBook-Pro-5.local>
2019-04-30 16:38:23 -0700

    complete

--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -1,5 +1,20 @@
 import { Router } from 'express'
 
+const controller = (req, res) => {
+  res.send({ message: 'hello' })
+}
+
 const router = Router()
+// '/api/item'
+router
+  .route('/')
+  .get(controller)
+  .post(controller)
+
+router
+  .route('/:id')
+  .put(controller)
+  .delete(controller)
+  .get(controller)
 
 export default router
